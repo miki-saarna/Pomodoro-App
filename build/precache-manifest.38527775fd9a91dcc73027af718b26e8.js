fc33445236dd04012963e88efdc2c1c03a909174
Mikito Saarna <mikitosaarna@gmail.com>
2021-09-26 17:12:22 -0500

    create predeployment and deployment hooks to evaluate all tests to passing before deploying to production

---
+++ build/precache-manifest.38527775fd9a91dcc73027af718b26e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9878d2260a1ffd11bff70c8de82d3c9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7065c0ff9c9ea6a3c6a9",
+    "url": "/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "223b709c9b9c0b308480",
+    "url": "/static/js/2.dcb25545.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.dcb25545.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7065c0ff9c9ea6a3c6a9",
+    "url": "/static/js/main.5762a00b.chunk.js"
+  },
+  {
+    "revision": "facd31b36309f08ff9ad",
+    "url": "/static/js/runtime-main.acde468f.js"
+  }
+]);
